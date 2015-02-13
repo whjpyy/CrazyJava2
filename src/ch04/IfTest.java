c3e0fd827fc07a1fe13b087832b9e3d3ee5c0646
Rebort Chen <409121961@qq.com>
2015-02-13 10:31:50 +0800

    if判断语句

---
+++ src/ch04/IfTest.java
@@ -0,0 +1,13 @@
+package ch04;
+
+/**
+ * Created by YouZeng on 2015-02-13.
+ */
+public class IfTest {
+    public static void main(String[] args) {
+        int age = 30;
+        if(age > 20){
+            System.out.println("年龄已经大于20岁了。");
+        }
+    }
+}
